64658e42b7cab91fb77298ecb55009abf77d5754
Nana <nana.d@hotmail.de>
2019-02-20 13:31:09 +0100

    added exercises

---
+++ Palindrome.java
@@ -0,0 +1,8 @@
+public class Palindrome{
+
+	//todo: Create a method isPalindrome which takes a String as input and returns true if it is a palindrome and false otherwise.
+	//A palindrome is a word which can be read backwards and forwards in the same way., e.g. 'hannah'.
+	//Ignore upper and lower case. 'Hannah' is a palindrome, too.
+	//Ignore also spaces. 'Taco cat' is a valid palindrome.
+
+}
